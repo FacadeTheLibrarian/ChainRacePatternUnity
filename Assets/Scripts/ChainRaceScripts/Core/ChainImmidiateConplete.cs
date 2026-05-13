a4c881a9258863c3ec5a7d01200f9b0afad77145
Yadori_Ouchi <yadoriouchi@gmail.com>
2026-05-13 14:38:15 +0900

    refactor: renaming ChainNop

---
+++ Assets/Scripts/ChainRaceScripts/Core/ChainImmidiateConplete.cs
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Kenichi Morishita
+
+namespace ChainPattern
+{
+    // Nop seems like "No operation "and never completes"", just like ChainFreeze(old -> ChainHalt)
+    // But I think "Complete immediately" is easier to understand, so I renamed it.
+    /// <summary>
+    /// Chain class that completes immediately after starting (no operation)
+    /// </summary>
+    public class ChainImmidiateConplete : BaseChain
+    {
+        /// <summary>
+        /// Starts execution
+        /// </summary>
+        protected override void StartInternal()
+        {
+            Complete();
+        }
+
+        /// <summary>
+        /// Called when skipped
+        /// </summary>
+        protected override void SkipInternal()
+        {
+        }
+    }
+}
