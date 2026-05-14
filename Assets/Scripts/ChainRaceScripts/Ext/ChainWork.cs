d2732bda944ad9e47346cd477f5d1667d5ef1b4c
Yadori_Ouchi <yadoriouchi@gmail.com>
2026-05-14 11:39:01 +0900

    refactor: add default null, add comments, and align code standard to original

--- Assets/Scripts/ChainRaceScripts/Ext/ChainWork.cs
+++ Assets/Scripts/ChainRaceScripts/Ext/ChainWork.cs
@@ -8,24 +8,24 @@ using System.Threading;
 namespace ChainPattern {
     //NOTE: せっかくChainParallelが存在するのにevent Actionでマルチキャストデリゲートするか？と思ったのでフックを外注製に
     /// <summary>
-    /// Chain with onStart, onSkip, onUpdate events
+    /// Chain with onStart, onSkip, onUpdate hooks
     /// </summary>
     public class ChainWork : BaseChain {
-        private IChainWorkLifeCycle _lifeCycle = default;
-        private CancellationTokenSource _tokenSource = default;
-        private bool _isDispatched = false;
+        IChainWorkLifeCycle lifeCycle = default;
+        CancellationTokenSource tokenSource = default;
+        bool isDispatched = false;
 
-        public ChainWork(IChainWorkLifeCycle lifeCycle) {
-            _lifeCycle = lifeCycle;
+        public ChainWork(IChainWorkLifeCycle lifeCycle = null) {
+            this.lifeCycle = lifeCycle;
         }
 
         /// <summary>
         /// Ends the work execution
         /// </summary>
         public void End() {
-            if (_isDispatched) {
-                _tokenSource?.Cancel();
-                _isDispatched = false;
+            if (isDispatched) {
+                tokenSource?.Cancel();
+                isDispatched = false;
                 Complete();
             }
         }
@@ -34,19 +34,23 @@ namespace ChainPattern {
         /// Starts execution
         /// </summary>
         protected override void StartInternal() {
-            _isDispatched = true;
-            _tokenSource = new CancellationTokenSource();
-            _lifeCycle?.BeforeStart();
-            FrameLoopAsync(_tokenSource.Token).Forget();
+            isDispatched = true;
+            // NOTE: if _lifeCycle is null, it means the user doesn't want to call _lifeCycle.Update(), just waiting for End()
+            //       so, no need to start FrameLoopAsync in that case
+            if (lifeCycle != null) {
+                tokenSource = new CancellationTokenSource();
+                lifeCycle.BeforeStart();
+                FrameLoopAsync(tokenSource.Token).Forget();
+            }
         }
 
         /// <summary>
         /// Called when skipped
         /// </summary>
         protected override void SkipInternal() {
-            _isDispatched = false;
-            _tokenSource?.Cancel();
-            _lifeCycle?.AfterSkip();
+            isDispatched = false;
+            tokenSource?.Cancel();
+            lifeCycle?.AfterSkip();
         }
 
         /// <summary>
@@ -55,10 +59,9 @@ namespace ChainPattern {
         private async UniTask FrameLoopAsync(CancellationToken token) {
             try {
                 while (!token.IsCancellationRequested) {
-                    bool shouldEnd = false;
-                    if (_lifeCycle != null) {
-                        shouldEnd = _lifeCycle.Update();
-                    }
+                    //NOTE: FrameLoopAsync should be called from StartInternal and _lifeCycle is guaranteed to be non-null in that case
+                    //      so no need to check for null here
+                    bool shouldEnd = lifeCycle.Update();
                     if (shouldEnd) {
                         End();
                     }
@@ -70,8 +73,8 @@ namespace ChainPattern {
             }
             finally {
                 // Dispose of resources after completion or cancellation
-                _tokenSource?.Dispose();
-                _tokenSource = null;
+                tokenSource?.Dispose();
+                tokenSource = null;
             }
         }
     }
